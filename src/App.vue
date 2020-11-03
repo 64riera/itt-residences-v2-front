ccda5b03c909c5dce8fbe7fccb85c695dd987240
Sergio Rivera <nokiacorporation1@gmail.com>
2020-11-02 18:06:00 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app id="app">
+    <router-view/>
+  </v-app>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
